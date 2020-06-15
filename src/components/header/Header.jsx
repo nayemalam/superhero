f5b527405e78e3f2f851a04c8aed267d90e728f0
Nayem <nayem.alam@mail.mcgill.ca>
2020-06-15 19:11:01 -0400

    tweaked main page styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import superhero from '../../images/superhero-img.jpg';
+import superhero from '../../images/superhero-img.png';
 
 class Header extends Component {
     render() {
