96878691dbee74fc0048ad1f66bd8123e0f02e9c
Nayem <nayem.alam@mail.mcgill.ca>
2020-06-30 10:50:17 -0400

    Updates

---
+++ precache-manifest.615e976f4aeba0489ca96571604a2c3b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e36023c865c62bc677911e104a56423",
+    "url": "/superhero/index.html"
+  },
+  {
+    "revision": "4e26de7ea9f3e7a0b0ec",
+    "url": "/superhero/static/css/2.2b06a122.chunk.css"
+  },
+  {
+    "revision": "39f0248675a956866e88",
+    "url": "/superhero/static/css/main.0e53d564.chunk.css"
+  },
+  {
+    "revision": "4e26de7ea9f3e7a0b0ec",
+    "url": "/superhero/static/js/2.a687d70f.chunk.js"
+  },
+  {
+    "revision": "a6130c7ab4f488d494df37df4126bb6f",
+    "url": "/superhero/static/js/2.a687d70f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39f0248675a956866e88",
+    "url": "/superhero/static/js/main.2796cd62.chunk.js"
+  },
+  {
+    "revision": "b56ef709fcb1d612686c",
+    "url": "/superhero/static/js/runtime-main.65f5ab43.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/superhero/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/superhero/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/superhero/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/superhero/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "6e72e46e7cd81acc95e28cdd5afe5724",
+    "url": "/superhero/static/media/superhero-img.6e72e46e.png"
+  }
+]);
